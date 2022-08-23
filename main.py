ff8bdb1e803d8cec27338aaa8c84f17f8dcdf629
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-08-23 09:34:22 -0700

    Comments and filtered dataframe example

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+#  Project from Codecademy involving analysis of U.S. Medical Insurance Data
+#  Collaboration with DenisLazuk and Olubunmi-Amoke
+
+# -----------------------------------------------------------
+
+#  Using pandas to read in the data frame.
 import pandas as pd
+# -----------------------------------------------------------
 
-dataset = pd.read_csv('insurance.csv')
-print(dataset)
 
-cl
+#  Read in dataframe from csv file and print header to inspect data.
+df = pd.read_csv('insurance.csv')
+#print(df.head())
+
+# -----------------------------------------------------------
+#  Creating a separate dataframe demonstrating how to filter values from a category.
+females = df[df.sex=='female']
+print(females)
+
